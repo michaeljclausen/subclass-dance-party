23332841e8bd6662cafe6f086d6f238e0f0ffe08
Hack Reactor Student <student@hackreactor.com>
2017-05-23 16:19:36 -0700

    Implement push apart dancers and css changes

--- src/init.js
+++ src/init.js
@@ -25,45 +25,51 @@ $(document).ready(function() {
     var dancer = new dancerMakerFunction(
       $('body').height() / 3 * Math.random() + 250,
       $('body').width() * Math.random(),
-      Math.random() * 1000
+      Math.random() * 1000 + 600
     );
     window.dancers.push(dancer);
-    // dancer.$node.mouseover(function() {
-    //   $(this).remove();
-    // });  
+    dancer.$node.mouseover(function() {
+      $(this).remove();
+    });  
 
     $('body').append(dancer.$node);
   });
   $('.lineUpDancers').on('click', function(event) {
     for (var i = 0; i < window.dancers.length; i++) {
       window.dancers[i].$node.addClass('lineUp');
-      window.dancers[i].$node.animate({ left: 10 }, 1000, function() {});
+      window.dancers[i].$node.animate({ top: 400 }, 1000, function() {});
     }
   });
   
   $('.pushApartDancers').on('click', function(event) {
-
-    var currentClosestDistance;
-    var closestIndexes;
+    debugger;
+    var currentClosestDistance,
+      closestIndexes;
     
     for (var i = 0; i < window.dancers.length; i++) {
-      var firstLeftPosition = window.dancers[i].position().left;
-      var firstTopPosition = window.dancers[i].position().top; 
+      if (window.dancers[i].$node.position().left === 10 || window.dancers[i].$node.position().left === 1400) {
+        continue;
+      }
+      var firstLeftPosition = window.dancers[i].$node.position().left;
+      var firstTopPosition = window.dancers[i].$node.position().top; 
       
-      for (var j = i; j < window.dancers.length; j++) {
-        var secondLeftPosition = window.dancers[j].position().left;
-        var secondTopPosition = window.dancers[j].position().top; 
+      for (var j = i + 1; j < window.dancers.length; j++) {
+        var secondLeftPosition = window.dancers[j].$node.position().left;
+        var secondTopPosition = window.dancers[j].$node.position().top; 
         var left = Math.abs(secondLeftPosition - firstLeftPosition);
         var top = Math.abs(secondTopPosition - firstTopPosition);
         
-        if (currentClosest === undefined || Math.pow(top, 2) + Math.pow(left, 2) > currentClosestDistance) {
+        if (currentClosestDistance === undefined || Math.pow(top, 2) + Math.pow(left, 2) < currentClosestDistance) {
           currentClosestDistance = Math.pow(top, 2) + Math.pow(left, 2);
           closestIndexes = [i, j];
         }
       }
-      window.dancers[i].$node.animate({ left: 10 }, 1000, function() {});
-      window.dancers[j].$node.animate({ left: -10 }, 1000, function() {});
     }
+    
+    window.dancers[closestIndexes[0]].$node.addClass('lineUp');   
+    window.dancers[closestIndexes[1]].$node.addClass('lineUp');  
+    window.dancers[closestIndexes[0]].$node.animate({ left: 10 }, 1000, function() {});
+    window.dancers[closestIndexes[1]].$node.animate({ left: 1400 }, 1000, function() {});
   });  
 
 });
